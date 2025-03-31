f1a5da22df2b12b1cbc0400d5dbeb90575cc0b27
cdnbelgium <jscharlach@hotmail.com>
2025-03-31 11:26:57 +0200

    put changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,12 +25,12 @@ const userDataPath = process.env.USER_DATA_PATH || path.resolve(__dirname);
 
 const app = express();
 
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
-// app.use(express.json());
+app.use(express.json());
 
 const PORT = 2756;
 
